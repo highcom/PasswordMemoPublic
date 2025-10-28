6f8627cda4f1900d6506bf5c3a918c7de88eaf3b
koichi highcom <youhappywith@yahoo.co.jp>
2025-10-28 12:17:01 +0900

    play-services-adsのバージョンアップ対応
     - バージョン21がsunsetするので関連するライブラリのバージョンも対応
     - adsは23系がkotlinのv1.9系でビルド可能

--- app/src/main/java/com/highcom/passwordmemo/data/PasswordMemoRoomDatabase.kt
+++ app/src/main/java/com/highcom/passwordmemo/data/PasswordMemoRoomDatabase.kt
@@ -10,11 +10,11 @@ import androidx.sqlite.db.SupportSQLiteDatabase
  * SQLiteからRoomへのマイグレーション操作
  */
 val MIGRATION_2_3 = object : Migration(2, 3) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.beginTransaction()
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.beginTransaction()
         try {
             // 新しいテーブルを一時テーブルとして構築(passworddata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE passworddata_tmp(
                     id INTEGER PRIMARY KEY NOT NULL,
                     title TEXT NOT NULL DEFAULT '',
@@ -28,18 +28,18 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
                 """.trimIndent()
             )
             // 旧テーブルのデータを全て一時テーブルに追加
-            database.execSQL("""
+            db.execSQL("""
                 INSERT OR IGNORE INTO passworddata_tmp (id,title,account,password,url,memo,inputdate)
                 SELECT id,title,account,password,url,memo,inputdate FROM passworddata
                 """.trimIndent()
             )
             // 旧テーブルを削除
-            database.execSQL("DROP TABLE passworddata")
+            db.execSQL("DROP TABLE passworddata")
             // 新テーブルをリネーム
-            database.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
+            db.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
 
             // 新しいテーブルを構築(groupdata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE groupdata(
                     group_id INTEGER PRIMARY KEY NOT NULL,
                     group_order INTEGER NOT NULL,
@@ -48,14 +48,14 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
                 """.trimIndent()
             )
             // レコードを作成
-            database.execSQL("""
+            db.execSQL("""
                 INSERT INTO groupdata VALUES(1, 1, 'すべて')
                 """.trimIndent()
             )
 
-            database.setTransactionSuccessful()
+            db.setTransactionSuccessful()
         } finally {
-            database.endTransaction()
+            db.endTransaction()
         }
     }
 }
@@ -65,11 +65,11 @@ val MIGRATION_2_3 = object : Migration(2, 3) {
  */
 val MIGRATION_3_4 = object : Migration(3, 4) {
     @SuppressLint("Range")
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.beginTransaction()
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.beginTransaction()
         try {
             // 新しいテーブルを一時テーブルとして構築(passworddata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE passworddata_tmp(
                     id INTEGER PRIMARY KEY NOT NULL,
                     title TEXT NOT NULL DEFAULT '',
@@ -86,11 +86,11 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
             // 以前のバージョンではidがプライマリキーになってないので重複していた可能性がある
             // なので改めて1から順番にIDを再設定する
             var newId = 1
-            val cursor = database.query("SELECT id FROM passworddata ORDER BY id;")
+            val cursor = db.query("SELECT id FROM passworddata ORDER BY id;")
 
             while (cursor.moveToNext()) {
                 val currentId = cursor.getInt(cursor.getColumnIndex("id"))
-                database.execSQL(
+                db.execSQL(
                     "UPDATE passworddata SET id = ? WHERE id = ?",
                     arrayOf<Any>(newId, currentId)
                 )
@@ -99,18 +99,18 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
             cursor.close()
 
             // 旧テーブルのデータを全て一時テーブルに追加
-            database.execSQL("""
+            db.execSQL("""
                 INSERT OR IGNORE INTO passworddata_tmp (id,title,account,password,url,group_id,memo,inputdate)
                 SELECT id,title,account,password,url,group_id,memo,inputdate FROM passworddata
                 """.trimIndent()
             )
             // 旧テーブルを削除
-            database.execSQL("DROP TABLE passworddata")
+            db.execSQL("DROP TABLE passworddata")
             // 新テーブルをリネーム
-            database.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
+            db.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
 
             // 新しいテーブルを一時テーブルとして構築(groupdata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE groupdata_tmp(
                     group_id INTEGER PRIMARY KEY NOT NULL,
                     group_order INTEGER NOT NULL,
@@ -119,19 +119,19 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
                 """.trimIndent()
             )
             // 旧テーブルのデータを全て一時テーブルに追加
-            database.execSQL("""
+            db.execSQL("""
                 INSERT INTO groupdata_tmp (group_id,group_order,name)
                 SELECT group_id,group_order,name FROM groupdata
                 """.trimIndent()
             )
             // 旧テーブルを削除
-            database.execSQL("DROP TABLE groupdata")
+            db.execSQL("DROP TABLE groupdata")
             // 新テーブルをリネーム
-            database.execSQL("ALTER TABLE groupdata_tmp RENAME TO groupdata")
+            db.execSQL("ALTER TABLE groupdata_tmp RENAME TO groupdata")
 
-            database.setTransactionSuccessful()
+            db.setTransactionSuccessful()
         } finally {
-            database.endTransaction()
+            db.endTransaction()
         }
     }
 }
@@ -141,11 +141,11 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
  */
 val MIGRATION_4_5 = object : Migration(4, 5) {
     @SuppressLint("Range")
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.beginTransaction()
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.beginTransaction()
         try {
             // 新しいテーブルを一時テーブルとして構築(passworddata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE passworddata_tmp(
                     id INTEGER PRIMARY KEY NOT NULL,
                     title TEXT NOT NULL DEFAULT '',
@@ -161,18 +161,18 @@ val MIGRATION_4_5 = object : Migration(4, 5) {
             )
 
             // 旧テーブルのデータを全て一時テーブルに追加
-            database.execSQL("""
+            db.execSQL("""
                 INSERT OR IGNORE INTO passworddata_tmp (id,title,account,password,url,group_id,memo,inputdate)
                 SELECT id,title,account,password,url,group_id,memo,inputdate FROM passworddata
                 """.trimIndent()
             )
             // 旧テーブルを削除
-            database.execSQL("DROP TABLE passworddata")
+            db.execSQL("DROP TABLE passworddata")
             // 新テーブルをリネーム
-            database.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
+            db.execSQL("ALTER TABLE passworddata_tmp RENAME TO passworddata")
 
             // 新しいテーブルを一時テーブルとして構築(groupdata)
-            database.execSQL("""
+            db.execSQL("""
                 CREATE TABLE groupdata_tmp(
                     group_id INTEGER PRIMARY KEY NOT NULL,
                     group_order INTEGER NOT NULL,
@@ -182,19 +182,19 @@ val MIGRATION_4_5 = object : Migration(4, 5) {
                 """.trimIndent()
             )
             // 旧テーブルのデータを全て一時テーブルに追加
-            database.execSQL("""
+            db.execSQL("""
                 INSERT INTO groupdata_tmp (group_id,group_order,name)
                 SELECT group_id,group_order,name FROM groupdata
                 """.trimIndent()
             )
             // 旧テーブルを削除
-            database.execSQL("DROP TABLE groupdata")
+            db.execSQL("DROP TABLE groupdata")
             // 新テーブルをリネーム
-            database.execSQL("ALTER TABLE groupdata_tmp RENAME TO groupdata")
+            db.execSQL("ALTER TABLE groupdata_tmp RENAME TO groupdata")
 
-            database.setTransactionSuccessful()
+            db.setTransactionSuccessful()
         } finally {
-            database.endTransaction()
+            db.endTransaction()
         }
     }
 }
